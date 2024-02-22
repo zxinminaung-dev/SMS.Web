ea079f869ed705a9efb8ee01218ddeea7da66b32
zxinminaung-dev <146321635+zxinminaung-dev@users.noreply.github.com>
2024-02-22 13:48:28 +0630

    commit

--- WebApplication1/obj/Debug/netcoreapp3.1/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/netcoreapp3.1/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+824408abaab617cbdacd3e880865d89bbbbfd2b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
